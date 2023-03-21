0b431c6a619e1c41fe5886eb42279567b6dda54a
jochen <jochen_sambaer@hotmail.com>
2023-03-21 11:45:08 +0100

    Intial commit

---
+++ labo11/oefening1.py
@@ -0,0 +1,4 @@
+#Schrijf een programma dat een integer getal vraagt aan de gebruiker. Jouw
+#programma geeft dan een boodschap terug met of het hier om een even of oneven
+#getal gaat. Tip: gebruik de modulus operator (%) om te bepalen of een getal even of
+#oneven is.
