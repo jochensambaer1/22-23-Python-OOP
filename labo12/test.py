6b2874e29dd2b3d00ca362f29717f406b8516b59
jochen <jochen_sambaer@hotmail.com>
2023-05-22 19:41:38 +0200

    uzers

--- labo12/test.py
+++ labo12/test.py
@@ -3,24 +3,31 @@ import os
 
 print(os.getcwd())  # print current working directory
 
-
 # Load and parse the geojson file
-with open('labo12/velo.geojson') as f:
+with open('velo.geojson') as f:
     data = geojson.load(f)
-
 
 # Extract data from the features
 rows = []
 for feature in data['features']:
     properties = feature['properties']
-    name = properties.get('name', 'unknown')
+    location_name = properties.get('Naam', 'unknown')
+    num_places = properties.get('Aantal_plaatsen', 'unknown')
+    location_type = properties.get('Objecttype', 'unknown')
+    location_use = properties.get('Gebruik', 'unknown')
+    location_street = properties.get('Straatnaam', 'unknown')
+    location_number = properties.get('Huisnummer', 'unknown')
+    location_district = properties.get('District', 'unknown')
+    location_postcode = properties.get('Postcode', 'unknown')
+    geometry = feature.get('geometry', 'unknown')
+    type = feature.get('type', 'unknown')
+    coordinates = geometry.get('coordinates', 'unknown')
 
-    capacity = properties.get('capacity', 'unknown')
-
-    rows.append('<tr><td>{}</td><td>{}</td></tr>'.format(name, capacity))
+    rows.append('<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'.format(
+        location_name, num_places, location_type, location_use, location_street, location_number, location_district, location_postcode))
 
 # Create the HTML table
-table = '<table><tr><th>Name</th><th>Capacity</th></tr>{}</table>'.format(''.join(rows))
+table = '<table><tr><th>Name</th><th>Capacity</th><th>Type</th><th>Use</th><th>Street</th><th>Number</th><th>District</th><th>Postcode</th></tr>{}</table>'.format(''.join(rows))
 
 # Write the HTML table to an output file
 with open('output.html', 'w') as f:
--- labo12/transporter.py
+++ labo12/transporter.py
@@ -1,7 +1,20 @@
+import geojson
+
+class User:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+
 class Transporter(User):
+    id = 0
+    state = "available"
+
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.bikes = []
+        self.id = Transporter.id
+        Transporter.id += 1
 
     def collect_bikes(self, station):
         for i in range(station.capacity):
@@ -15,3 +28,223 @@ class Transporter(User):
                 self.bikes[i].station = station
                 self.bikes.pop(i)
                 i -= 1
+
+if __name__ == '__main__':
+    transporter1 = Transporter("John", "Doe")
+    properties = {"id": transporter1.id, "state": transporter1.state}
+    print(properties)  # Output: {'id': 0, 'state': 'available'}
+
+
+
+                
+# Create a list to store the features
+Transporters = [
+    Transporter(1, "available"),
+    Transporter(1, "available"),
+    Transporter(2, "available"),
+    Transporter(3, "borrowed"),
+    Transporter(1, "available"),
+    Transporter(2, "available"),
+    Transporter(3, "borrowed"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(4, "available"),
+    Transporter(5, "borrowed")
+]
+features = []
+
+# Loop over each bike object and create a feature with its location and state
+for Transporter in Transporters:
+    point = geojson.Point((0, 0))  # Replace with actual coordinates
+    properties = {"id": Transporter.id, "state": Transporter.state}
+    feature = geojson.Feature(geometry=point, properties=properties)
+    features.append(feature)
+
+# Create a FeatureCollection with the features
+feature_collection = geojson.FeatureCollection(features)
+
+# Write the FeatureCollection to a GeoJSON file
+with open("Transporters.geojson", "w") as f:
+    geojson.dump(feature_collection, f)
