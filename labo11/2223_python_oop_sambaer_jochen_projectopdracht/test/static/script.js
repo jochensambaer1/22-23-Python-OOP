d1c26e5f66ddcd517e6447c3b761b1888315ddf2
jochen <jochen_sambaer@hotmail.com>
2023-05-29 15:55:32 +0200

    updatsim

--- labo11/2223_python_oop_sambaer_jochen_projectopdracht/test/static/script.js
+++ labo11/2223_python_oop_sambaer_jochen_projectopdracht/test/static/script.js
@@ -1,19 +1,18 @@
 // Get the form and name list elements
-const nameForm = document.getElementById('nameForm');
-const nameList = document.getElementById('nameList');
-
+const nameForm = document.getElementById("nameForm");
+const nameList = document.getElementById("nameList");
 
 // Fetch bikes.geojson and populate the bike table
 fetch("/static/bikes.geojson")
   .then((response) => response.json())
   .then((bikeData) => {
-    const bikeTableBody = document.getElementById('bikeTableBody');
+    const bikeTableBody = document.getElementById("bikeTableBody");
     bikeData.features.forEach((feature) => {
       const row = document.createElement("tr");
       row.innerHTML = `
         <td>${feature.properties.id}</td>
         <td>${feature.properties.state}</td>
-        <td>${feature.properties.borrow_time || ''}</td>
+        <td>${feature.properties.borrow_time || ""}</td>
         <td>${feature.geometry.coordinates[1]}</td>
         <td>${feature.geometry.coordinates[0]}</td>
       `;
@@ -22,10 +21,10 @@ fetch("/static/bikes.geojson")
   })
   // Fetch velo.geojson and populate the station table
   .catch((error) => console.log(error));
-  fetch("/static/velo.geojson")
+fetch("/static/velo.geojson")
   .then((response) => response.json())
   .then((stationData) => {
-    const stationTableBody = document.getElementById('stationTableBody');
+    const stationTableBody = document.getElementById("stationTableBody");
     stationData.features.forEach((feature) => {
       const row = document.createElement("tr");
       row.innerHTML = `
@@ -50,11 +49,10 @@ fetch("/static/bikes.geojson")
   })
   .catch((error) => console.log(error));
 
-
-  fetch("/static/name.geojson")
+fetch("/static/name.geojson")
   .then((response) => response.json())
   .then((userData) => {
-    const userTableBody = document.getElementById('userTableBody');
+    const userTableBody = document.getElementById("userTableBody");
     userData.forEach((user) => {
       const row = document.createElement("tr");
       row.innerHTML = `
@@ -72,64 +70,70 @@ fetch("/static/bikes.geojson")
   })
   .catch((error) => console.log(error));
 
-
 // Fetch transporters.geojson and populate the transporters table
-fetch("/static/Transporters.geojson")
-.then((response) => response.json())
-.then((userData) => {
-  const BikeTransporterTableBody = document.getElementById('BikeTransporterTableBody');
-  userData.forEach((user) => {
-    const row = document.createElement("tr");
-    row.innerHTML = `
-      <td>${user.id}</td>
-      <td>${user.first_name}</td>
-      <td>${user.last_name}</td>
-      <td>${user.full_name}</td>
-      <td>${user.time_biking_minutes}</td>
-      <td>${user.time_biking_hours}</td>
-      <td>${user.total_time_biking_minutes}</td>
-      <td>${user.saldo}</td>
-    `;
-    BikeTransporterTableBody.appendChild(row);
-  });
-})
-.catch((error) => console.log(error));
+
+
+  fetch("/static/Transporters.geojson")
+  .then((response) => response.json())
+  .then((BikeTransporter) => {
+    const BikeTransporterTableBody = document.getElementById("BikeTransporterTableBody");
+    BikeTransporter.forEach((BikeTransporter) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${BikeTransporter.id}</td>
+        <td>${BikeTransporter.first_name}</td>
+        <td>${BikeTransporter.last_name}</td>
+        <td>${BikeTransporter.full_name}</td>
+        <td>${BikeTransporter.time_biking_minutes}</td>
+        <td>${BikeTransporter.time_biking_hours}</td>
+        <td>${BikeTransporter.total_time_biking_minutes}</td>
+        <td>${BikeTransporter.saldo}</td>
+      `;
+      BikeTransporterTableBody.appendChild(row);
+    });
+  })
+  .catch((error) => console.log(error));
 
 // Function to fetch the names JSON file and generate a random name
 async function generateRandomNames(count) {
   try {
-    console.log('Fetching names...');
-    const response = await fetch('/static/names.json');
-    console.log('Got names!');
+    console.log("Fetching names...");
+    const response = await fetch("/static/names.json");
+    console.log("Got names!");
     const data = await response.json();
 
     const randomNames = [];
     for (let i = 0; i < count; i++) {
-      const randomFirstNameIndex = Math.floor(Math.random() * data.first_name.length);
+      const randomFirstNameIndex = Math.floor(
+        Math.random() * data.first_name.length
+      );
       const randomFirstName = data.first_name[randomFirstNameIndex];
 
-      const randomLastNameIndex = Math.floor(Math.random() * data.last_name.length);
+      const randomLastNameIndex = Math.floor(
+        Math.random() * data.last_name.length
+      );
       const randomLastName = data.last_name[randomLastNameIndex];
 
-      const fullName = randomFirstName + ' ' + randomLastName;
+      const fullName = randomFirstName + " " + randomLastName;
       randomNames.push(fullName);
     }
 
     // Create a data blob with the random names
-    const dataBlob = new Blob([JSON.stringify(randomNames)], { type: 'application/json' });
+    const dataBlob = new Blob([JSON.stringify(randomNames)], {
+      type: "application/json",
+    });
 
     // Create a download link
-    const downloadLink = document.createElement('a');
+    const downloadLink = document.createElement("a");
     downloadLink.href = URL.createObjectURL(dataBlob);
-    downloadLink.download = 'random_names.json';
+    downloadLink.download = "random_names.json";
 
     // Append the download link to the document body and trigger the click event
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
   } catch (error) {
-    console.error('Error fetching names: ' + error);
-    throw new Error('Error fetching names: ' + error);
+    console.error("Error fetching names: " + error);
+    throw new Error("Error fetching names: " + error);
   }
 }
-
--- labo11/2223_python_oop_sambaer_jochen_projectopdracht/test/web.py
+++ labo11/2223_python_oop_sambaer_jochen_projectopdracht/test/web.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, redirect, url_for, render_template
 import json
 import random
 import logging
+import geojson
+import os
+
+
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -66,6 +70,29 @@ class User:
         self.bike = None
 
         logger.debug(f"Created user {self.user_id} with name {self.full_name} and saldo {self.saldo}")
+class Transporter(User):
+    id = 0
+    state = "available"
+
+    def __init__(self, first_name, last_name):
+        super().__init__(Transporter.id, first_name, last_name, f"{first_name} {last_name}", 0, 0, 0)
+        self.bikes = []
+        self.id = Transporter.id
+        Transporter.id += 1
+
+
+    def collect_bikes(self, station):
+        for i in range(station.capacity):
+            if station.slots[i] is not None and station.slots[i].state == "available":
+                self.bikes.append(station.slots[i])
+                station.slots[i] = None
+
+    def distribute_bikes(self, station):
+        for i in range(len(self.bikes)):
+            if station.add_bike(self.bikes[i]):
+                self.bikes[i].station = station
+                self.bikes.pop(i)
+                i -= 1
 
 
 class BikeShareSystem:
@@ -117,75 +144,142 @@ def generate_random_bikes(num_bikes):
 
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
-@app.route('/generate_names')
-def generate_names():
-    num_names = int(request.args.get('num_names', 1000))  # Default to 10 names if not specified
+    return render_template('index.html')
+@app.route('/sim')
+def sim():
+    return render_template('sim.html')
 
-    with open('names.json', 'r', encoding='utf-8') as f:
-        names_data = json.load(f)
-
-    # Assuming names_data is a list of dictionaries, you can extract the first and last names
-    first_names = names_data['first_name']
-    last_names = names_data['last_name']
-
-    # Create a list of dictionaries to store the generated names
-    name_list = []
-
-    # Generate the specified number of names and add them to the list
-    for i in range(num_names):
-        # Select two random names from the first_names and last_names lists
-        first_name = random.choice(first_names)
-        last_name = random.choice(last_names)
-
-        # Combine the names to create a new name
-        full_name = f"{first_name} {last_name}"
-
-        # Create a unique ID for each person
-        person_id = i + 1
-
-        # Generate random values for time biking in minutes and hours
-        time_biking_minutes = random.randint(0, 59)
-        time_biking_hours = random.randint(0, 10)
-
-        # Calculate the total time biking in minutes
-        total_time_biking_minutes = time_biking_minutes + (time_biking_hours * 60)
-
-        # Generate a random saldo (account balance) with a maximum of 2 decimal places
-        saldo = round(random.uniform(0, 1000), 2)
-
-        # Create an empty bike object
-        bike = {}
-
-        # Add the new name to the list as a dictionary with additional fields
-        name_dict = {
-            'id': person_id,
-            'first_name': first_name,
-            'last_name': last_name,
-            'full_name': full_name,
-            'time_biking_minutes': time_biking_minutes,
-            'time_biking_hours': time_biking_hours,
-            'total_time_biking_minutes': total_time_biking_minutes,
-            'saldo': saldo,
-            'bike': bike
-        }
-
-        # Add the new person to the name_list
-        name_list.append(name_dict)
-
-    return jsonify(name_list)
 @app.route('/submit', methods=['POST'])
 def submit():
-    input_text = request.form['input_text']
     try:
-        input_int = int(input_text)
-        bike_share_system.users = generate_random_users(input_int)
-        bike_share_system.bikes = generate_random_bikes(input_int)
+        input_int = int(request.form['input_text'])
+        if 'generet users' in request.form:
+                    
+            num_names = int(request.args.get('num_names', input_int))  # Default to 10 names if not specified
+
+            with open('data/names.json', 'r', encoding='utf-8') as f:
+                names_data = json.load(f)
+
+
+            # Assuming names_data is a list of dictionaries, you can extract the first and last names
+            first_names = names_data['first_name']
+            last_names = names_data['last_name']
+
+            # Create a list of dictionaries to store the generated names
+            name_list = []
+
+            # Generate the specified number of names and add them to the list
+            for k in range(num_names):
+                # Select two random names from the first_names and last_names lists
+                first_name = random.choice(first_names)
+                last_name = random.choice(last_names)
+
+                # Combine the names to create a new name
+                full_name = f"{first_name} {last_name}"
+
+                # Create a unique ID for each person
+                person_id = k + 1
+
+                # Generate random values for time biking in minutes and hours
+                time_biking_minutes = random.randint(0, 59)
+                time_biking_hours = random.randint(0, 10)
+
+                # Calculate the total time biking in minutes
+                total_time_biking_minutes = time_biking_minutes + (time_biking_hours * 60)
+
+                # Generate a random saldo (account balance) with a maximum of 2 decimal places
+                saldo = round(random.uniform(0, 1000), 2)
+
+                # Create an empty bike object
+                bike = {}
+
+                # Add the new name to the list as a dictionary with additional fields
+                name_dict = {
+                    'id': person_id,
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'full_name': full_name,
+                    'time_biking_minutes': time_biking_minutes,
+                    'time_biking_hours': time_biking_hours,
+                    'total_time_biking_minutes': total_time_biking_minutes,
+                    'saldo': saldo,
+                    'bike': bike
+                }
+
+                # Add the new person to the name_list
+                name_list.append(name_dict)
+            with open('static/name.geojson', 'w') as f:        
+                # Write the list of names to a JSON file
+                with open('static/name.geojson', 'w') as f:
+                    json.dump(name_list, f)
+            return redirect(url_for('index'))  
+            
+        elif 'generat transporters' in request.form:
+                    
+            num_names = int(request.args.get('num_names', input_int))  # Default to 10 names if not specified
+
+            with open('data/names.json', 'r', encoding='utf-8') as f:
+                names_data = json.load(f)
+
+
+            # Assuming names_data is a list of dictionaries, you can extract the first and last names
+            first_names = names_data['first_name']
+            last_names = names_data['last_name']
+
+            # Create a list of dictionaries to store the generated names
+            name_list = []
+
+            # Generate the specified number of names and add them to the list
+            for k in range(num_names):
+                # Select two random names from the first_names and last_names lists
+                first_name = random.choice(first_names)
+                last_name = random.choice(last_names)
+
+                # Combine the names to create a new name
+                full_name = f"{first_name} {last_name}"
+
+                # Create a unique ID for each person
+                person_id = k + 1
+
+                # Generate random values for time biking in minutes and hours
+                time_biking_minutes = random.randint(0, 59)
+                time_biking_hours = random.randint(0, 10)
+
+                # Calculate the total time biking in minutes
+                total_time_biking_minutes = time_biking_minutes + (time_biking_hours * 60)
+
+                # Generate a random saldo (account balance) with a maximum of 2 decimal places
+                saldo = round(random.uniform(0, 1000), 2)
+
+                # Create an empty bike object
+                bike = {}
+
+                # Add the new name to the list as a dictionary with additional fields
+                name_dict = {
+                    'id': person_id,
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'full_name': full_name,
+                    'time_biking_minutes': time_biking_minutes,
+                    'time_biking_hours': time_biking_hours,
+                    'total_time_biking_minutes': total_time_biking_minutes,
+                    'saldo': saldo,
+                    'bike': bike
+                }
+
+                # Add the new person to the name_list
+                name_list.append(name_dict)
+            with open('static/Transporters.geojson', 'w') as f:        
+            # Write the list of names to a JSON file
+                with open('static/Transporters.geojson', 'w') as f:
+                    json.dump(name_list, f)
+            return redirect(url_for('index'))  
+        else:
+            return 'Unknown button clicked!'
     except ValueError:
-        return 'Invalid input!'
-    logger.debug(input_int)
+        return 'Invalid input: not a number!'
     
    
 
@@ -196,13 +290,17 @@ if __name__ == "__main__":
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
-    with open("velo.geojson") as file:
-        geojson_data = json.load(file)
+    with open("static/velo.geojson") as file:
+        velo_geojson_data = json.load(file)
+    with open("static/bikes.geojson") as file:
+         bike_geojson_data = json.load(file)
+    with open("static/name.geojson") as file:
+        user_geojson_data = json.load(file)
+    with open("static/Transporters.geojson") as file:
+        transporter_geojson_data = json.load(file)
 
-    bike_share_system = BikeShareSystem(geojson_data)
-    bike_share_system.users = generate_random_users(10)
-    bike_share_system.bikes = generate_random_bikes(10)
-
+    bike_share_system = BikeShareSystem(velo_geojson_data)
+   
     app.run(debug=True)
 
 
